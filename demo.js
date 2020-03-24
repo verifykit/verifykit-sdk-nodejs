cf0cdd38fc0c1872d43861bc915763620588d000
AhmtBrK <ahmtbrk58@gmail.com>
2020-03-24 10:09:09 +0300

    Added access token feature for the web

--- demo.js
+++ demo.js
@@ -2,7 +2,15 @@ const VerifyKit = require('verifykit')
 
 const verifyKit = new VerifyKit("server_key");
 
+// validate sessionId
 verifyKit.validate('session_id').then(response => {
+    console.log(response);
+}).catch(error => {
+    console.log(error)
+});
+
+// get web access token to use VerifyKit on web platforms
+verifyKit.getWebAccessToken().then(response => {
     console.log(response);
 }).catch(error => {
     console.log(error)
